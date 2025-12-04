0ce5fce60c82d18d15707d3a5a874525ed9a87ff
Bob Wakefield <bob@cockleburr.net>
2025-12-04 00:45:54 -0500

    Initial Commit

---
+++ GameControllerBehavior/ViewController.swift
@@ -0,0 +1,23 @@
+// ViewController.swift
+//
+// Created by Bob Wakefield on 12/4/25.
+// for GameControllerBehavior
+//
+// Using Swift 6.0
+// Running on macOS 26.1
+//
+// 
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GameControllerBehaviorTests/GameControllerBehaviorTests.swift
@@ -0,0 +1,21 @@
+// GameControllerBehaviorTests.swift
+//
+// Created by Bob Wakefield on 12/4/25.
+// for GameControllerBehavior
+//
+// Using Swift 6.0
+// Running on macOS 26.1
+//
+// 
+//
+
+import Testing
+@testable import GameControllerBehavior
+
+struct GameControllerBehaviorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
